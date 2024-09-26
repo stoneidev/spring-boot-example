0e5cba75e468f0537410705f3430e419299b3f6d
KIM JONG IL <124699796+stoneidev@users.noreply.github.com>
2024-09-26 23:02:28 +0900

    Update HealthController.java

--- src/main/java/com/stoeni/ecsdevops/ui/HealthController.java
+++ src/main/java/com/stoeni/ecsdevops/ui/HealthController.java
@@ -15,4 +15,16 @@ public class HealthController {
     public String healthcheck() {
         return "OK";
     }
+
+    @GetMapping("/delay")
+    public String delay() {
+        try {
+            // 0.04초(40밀리초) 동안 지연
+            Thread.sleep(40);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return "Error: Interrupted";
+        }
+        return "Delayed Response";
+    }
 }
